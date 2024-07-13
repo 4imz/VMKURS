826a24219af75cfb38575a4258cf88ee26244100
4imz <matveyvlog.su@gmail.com>
2024-07-13 19:32:33 +0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,179 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>RPCS3 - About</title>
+<meta charset="utf-8">
+<meta name="description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, about">
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="RPCS3 - About"/>
+<meta property="og:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering."/>
+<meta property="og:image" content="/img/meta/mobile/1200.png"/>
+<meta property="og:image:width" content="1200"/>
+<meta property="og:image:height" content="630"/>
+<meta property="og:url" content="https://rpcs3.net"/>
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website"/>
+<meta property="og:site_name" content="RPCS3"/>
+<meta name="twitter:title" content="RPCS3 - About">
+<meta name="twitter:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="twitter:image" content="/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-global.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>Отзывы</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Забота о клиентах</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Забота о клиентах - это не только важная часть успешного бизнеса, но и проявление нашего уважения и внимания к каждому человеку, обратившемуся к нам. Мы стремимся создать комфортную и надежную среду для наших клиентов, чтобы они чувствовали себя важными и удовлетворенными. <br>
+									<br>
+									 Наша забота начинается с того момента, как клиент связывается с нами. Мы гарантируем оперативный ответ на все запросы и вопросы, чтобы наши клиенты не чувствовали себя забытыми или игнорируемыми. Мы слушаем и стараемся понять потребности каждого клиента, чтобы предоставить решения, которые наиболее соответствуют их ожиданиям.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/RR.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-img-container splitter-con-left'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/porshe.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Стремление</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Мы также стремимся поддерживать постоянную связь с нашими клиентами. Мы предоставляем персональный подход и регулярно обновляем наших клиентов о новостях, акциях и скидках, которые могут быть для них интересными. Мы также прислушиваемся к обратной связи и мнениям наших клиентов, чтобы постоянно улучшать наши услуги и удовлетворять их потребности. <br>
+									<br>
+									 Кроме того, мы гарантируем безопасность и конфиденциальность информации наших клиентов. Мы строго соблюдаем все нормы и требования в отношении защиты данных, чтобы клиенты могли быть уверены в безопасности своих личных и финансовых данных.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Цель</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Наша цель - не только удовлетворить потребности клиентов, но и превзойти их ожидания. Мы стремимся предоставить высокое качество услуг, профессиональные консультации и поддержку на каждом этапе сотрудничества с нами. Мы гордимся возможностью заботиться о наших клиентах и делать все возможное, чтобы они чувствовали себя особенными и ценными для нас. <br>
+									<br>
+									 
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/h2team.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>Клиенты тарифа(Стандарт)</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+							 
+						</p>
+					</div>
+				</div>
+			</div>
+			<?php include 'lib/module/about/inc-about-founders.php';?>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>Клиенты тарифа(Премиум)</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+							 
+						</p>
+					</div>
+				</div>
+			</div>
+			<?php include 'lib/module/about/inc-about-current.php';?>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						
+						<h2></h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+							 
+						</p>
+					</div>
+				</div>
+			</div>
+			<?php include 'lib/module/about/inc-about-former.php';?>
+		</div>
+	</div>
+</div>
+</div>
+<?php include 'lib/module/inc-footer.php';?>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>RPCS3 - The PlayStation 3 Emulator</title>
+<meta charset="utf-8">
+<meta name="description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux, macOS and FreeBSD. The purpose of this project is to accurately emulate the PlayStation 3 in its entirety with the power of reverse engineering and community collaboration.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, home">
+<meta name="google-site-verification" content="cO1o6sx54cvKxhbnYsABWtl4sYFj9uVKV0DxLKZkWv8"/>
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="RPCS3 - The PlayStation 3 Emulator" />
+<meta property="og:description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux, macOS and FreeBSD made possible with the power of reverse engineering." />
+<meta property="og:image" content="/img/meta/mobile/1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://rpcs3.net" />
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="RPCS3" />
+<meta name="twitter:title" content="RPCS3 - The PlayStation 3 Emulator">
+<meta name="twitter:description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux, macOS and FreeBSD made possible with the power of reverse engineering.">
+<meta name="twitter:image" content="/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+<script data-cfasync="false" src='/lib/js/inc-carousel.js?v=1'></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6161192934701994" crossorigin="anonymous"></script>
+</head>
+<body>
+<img src="/img/graphics/landing/drift-car.png" width="1" height="1" alt="preload"/>
+<img src="/img/videos/1.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3.jpg" width="1" height="1" alt="Preload"/>
+<?php include 'lib/module/sys-global.php';?>
+<?php include 'lib/module/home/inc-home-player.php'?>
+<?php
+	preloadVideo(
+	'1',
+	'19ae5Mq2lJE',
+	'1'
+	);
+	preloadVideo(
+	'2',
+	'mKON3RwjnEU',
+	'2'
+	);
+	preloadVideo(
+	'3',
+	'xUpw9bHmoac',
+	'3'
+	);
+?>
+<div class="page-con-content landing-con-override">
+	<div class="wavebar-con-container-master">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+	
+	<div class="landing-con-main">
+		<div id="object-particles">
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class='landing-con-left content-expand '>
+					<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+						<div class='landing-ico-logo'>
+						</div>
+						<div class='landing-tx1-heading pulsate'>
+							<h1>КЛУБ МОЛОДЫХ МИЛЛИОНЕРОВ<br>
+							 </h1>
+						</div>
+						<div class='landing-tx2-heading'>
+							<p>
+								 Приобрести курс по ссылке ниже. Добро пожаловать в клуб.
+							</p>
+							<div class='landing-btn-container'>
+								<div class="build-btn-button">
+									<div class="build-ico-button">
+										<div class="build-ico-os">
+											<img alt="Windows" src="img/icons/list/korzina1.png" style='height: 120%; width: 100%; object-fit: contain'/>
+											<img alt="Linux" src="img/icons/list/korzina2.png" style='height: 120%; width: 100%; object-fit: contain; display:none;'/>
+											<img alt="macOS" src="img/icons/list/korzina3.png" style='height: 120%; width: 100%; object-fit: contain; display:none;'/>
+											<img alt="FreeBSD" src="img/icons/list/korzina4.png" style='height: 120%; width: 100%; object-fit: contain; display:none;'/>
+									</div>
+									</div>
+									<a href="/download">
+									<div class="build-tx1-button">
+										<span>купить</span>
+										<span class="build-tx2-label">4999руб</span>
+									</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='landing-con-right content-remove '>
+					<div class='landing-img-hero' style="background: url(/img/graphics/landing/drift-car.png) no-repeat center; background-size: contain;">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+		
+	
+		
+</div>
+</body>
+</html>
